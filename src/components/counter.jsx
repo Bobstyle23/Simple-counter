cf6d7220833af31f13fa469ce089a7b84b643477
Bob Pakhriev <boburpakhriev@gmail.com>
2021-07-03 10:36:54 +0900

    Initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Hello World</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Counter;
