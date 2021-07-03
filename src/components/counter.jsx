ce53414ebeaee15afe3b5c91f19689c41b2af37f
Bob Pakhriev <boburpakhriev@gmail.com>
2021-07-03 11:53:56 +0900

    Add styles to the component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,52 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 0,
+    image: "https://picsum.photos/400",
+    names: ["MukhammadBobur", "Bakhtiyor", "Mukhtasar", "Maftuna"],
+  };
+
   render() {
     return (
-      <div>
-        <header>
-          <h1>Hello World</h1>
-        </header>
-      </div>
+      <React.Fragment>
+        <figure>
+          <figcaption>
+            <img src={this.state.image} alt="" />
+            <p>These are the random photos</p>
+          </figcaption>
+        </figure>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.names.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
+      </React.Fragment>
     );
   }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  getBadgeClasses() {
+    const { count } = this.state;
+    let classes = "badge m-2 bg-";
+    classes += count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  handleIncrement = () => {
+    const { count } = this.state;
+    this.setState(count + 1);
+  };
 }
 
 export default Counter;
